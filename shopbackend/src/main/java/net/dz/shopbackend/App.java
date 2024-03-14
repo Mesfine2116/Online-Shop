6a63837c58b0c529941e29acf06bd49d5a500b93
Your Name <you@ex>
2024-03-14 10:24:21 +0300

    first

--- shopbackend/src/main/java/net/dz/shopbackend/App.java
+++ shopbackend/src/main/java/net/dz/shopbackend/App.java
@@ -4,16 +4,19 @@ package net.dz.shopbackend;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 
-public void firsMethod(String name, String  Id){
+    public void firsMethod(String name, String Id) {
 
-}
-public void secondMethod ()  {
-}
+    }
+
+    public void secondMethod() {
+    }
+
+    public void thirdMetohd() {
+        System.out.print();
+    }
 }
